cbe7c152c32774de07ff831e137fff7927ee9145
bhattaraijay05 <bhattaraijay23@gmail.com>
2021-07-23 17:15:54 +0530

    added

--- all ques/13aprog.c
+++ all ques/13aprog.c
@@ -1,3 +1,35 @@
 // Write a separate program using signal system call to catch the following signals.
 
 //     o SIGALRM
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void sigproc(void);
+
+void quitproc(void);
+
+main()
+{
+    signal(SIGINT, sigproc);
+    signal(SIGQUIT, quitproc);
+    printf("ctrl-c disabled use ctrl-\\ to quit\n");
+    for (;;)
+        ; /* infinite loop */
+}
+
+void sigproc()
+{
+    signal(SIGINT, sigproc); /*  */
+    /* NOTE some versions of UNIX will reset signal to default
+		 after each call. So for portability reset signal each time */
+
+    printf("you have pressed ctrl-c \n");
+}
+
+void quitproc()
+{
+    printf("ctrl-\\ pressed to quit\n");
+    exit(0); /* normal exit status */
+}
