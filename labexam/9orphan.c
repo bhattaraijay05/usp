9b8ac29007c11893b55cb8ba45324faca39e0334
Jay <bhattaraijay23@gmail.com>
2021-11-29 12:10:36 +0530

    changed

--- labexam/9orphan.c
+++ labexam/9orphan.c
@@ -1,19 +1,19 @@
-// 9.Write a C program that illustrates how an orphan is created.
-#include <stdio.h>
+#include<stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+  
 int main()
-{
-    int pid;
-    printf("I'am the original process with PID %d and PPID %d.\n", getpid(), getppid());
-    pid = fork();
-    if (pid != 0)
+{   
+    int pid = fork();
+  
+    if (pid > 0)
+        printf("in parent process");
+  
+    else if (pid == 0)
     {
-        printf("I'am the parent with PID %d and PPID %d.\n", getpid(), getppid());
-        printf("My child's PID is %d\n", pid);
+        sleep(30);
+        printf("in child process");
     }
-    else
-    {
-        sleep(4);
-        printf("I'm the child with PID %d and PPID %d.\n", getpid(), getppid());
-    }
-    printf("PID %d terminates.\n", getpid());
+  
+    return 0;
 }
